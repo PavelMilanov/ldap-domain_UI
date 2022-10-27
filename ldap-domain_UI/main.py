693b867a2cb3ebe72beba1417d99f4836f866cda
Pavel Milanov <milanov71@icloud.com>
2022-10-27 11:21:01 +0300

    init

--- ldap-domain_UI/main.py
+++ ldap-domain_UI/main.py
@@ -9,5 +9,5 @@ env.read_env()
 server = Server(env('DC'), get_info=ALL)
 conn = Connection(server, user=env('domain_user'), password=env('domain_password') , auto_bind=True, authentication=NTLM)
 
-conn.search(f"dc={env('domain_root')},dc={env('domain_forest')}", '(&(objectclass=person)(uid=Administrator))')
+conn.search(f"ou=Customer,ou=Customers,ou=FSO,dc={env('domain_root')},dc={env('domain_forest')}", '(&(objectclass=person)(cn=customer_test))', attributes=['*'])
 print(conn.entries)
